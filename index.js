1a9212cd7c40d42dac7d3678156e1193df6da51c
Nurbol876 <nurbolsagynbekov876@gmail.com>
2024-08-26 19:15:53 +0600

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/data', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: "Hello from Vercel!" });
 });
 
